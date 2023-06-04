a62b361870b5feb4bba990f534a684bfd5b037d4
Alexander Kurz <alex.kurz95@gmail.com>
2023-06-04 18:25:49 +0200

    Integrated ISIC model into swift app

--- swift/Extensions/VNClassificationObservation+confidenceString.swift
+++ swift/Extensions/VNClassificationObservation+confidenceString.swift
@@ -11,18 +11,20 @@ extension VNClassificationObservation {
     /// Generates a string of the observation's confidence as a percentage.
     var confidencePercentageString: String {
         let percentage = confidence * 100
+        
+        return String(format: "%3.2f", percentage)
 
-        switch percentage {
-            case 100.0...:
-                return "100%"
-            case 10.0..<100.0:
-                return String(format: "%2.1f", percentage)
-            case 1.0..<10.0:
-                return String(format: "%2.1f", percentage)
-            case ..<1.0:
-                return String(format: "%1.2f", percentage)
-            default:
-                return String(format: "%2.1f", percentage)
-        }
+//        switch percentage {
+//            case 100.0...:
+//                return "100%"
+//            case 10.0..<100.0:
+//                return String(format: "%2.1f", percentage)
+//            case 1.0..<10.0:
+//                return String(format: "%2.1f", percentage)
+//            case ..<1.0:
+//                return String(format: "%1.2f", percentage)
+//            default:
+//                return String(format: "%2.1f", percentage)
+//        }
     }
 }
